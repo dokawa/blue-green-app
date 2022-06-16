dbd49ad34a45b2a29e7c56699027698a40fa43f3
daniel <do@tsl.io>
2022-06-15 21:53:16 -0300

    [doc] Improve doc

--- requests.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-IP=${1}
-while true;do curl -s "$IP" --stderr - | grep -oP -e '(?<=--color:).*'; sleep 1; done
-# while true;do curl -s http://192.168.59.100:30955/; sleep 1; done
